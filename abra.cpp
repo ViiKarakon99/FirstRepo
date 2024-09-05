d69c0fbc43abac09732daae41552afd9424417c6
ViiKarakon99 <viladmirejalang1999@gmail.com>
2024-09-05 22:39:26 +0700

    big move

---
+++ abra.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int b;
+    int h;
+
+
+    cout<<"area of the rectangle \n";
+    cout<<"enter hieght :";
+    cin>>h;
+    cout<<"enter base :";
+    cin>>b;
+
+    cout<<"this is the area : " << b*h;
+
+    return 0;
+
+}
